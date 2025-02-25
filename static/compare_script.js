75552240ef65726f09c6ac17f40f68a38018e92f
Mukta9904 <muktadas9904@gmail.com>
2025-02-25 17:20:42 +0530

    Added the register and recognize routes

---
+++ static/compare_script.js
@@ -0,0 +1,50 @@
+
+const registerVideoButton = document.getElementById("takeVideo");
+const videoElement = document.createElement("video");
+videoElement.controls = true;
+document.body.appendChild(videoElement);
+
+registerVideoButton.addEventListener("click", () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      const mediaRecorder = new MediaRecorder(stream);
+      const chunks = [];
+
+      mediaRecorder.start();
+
+      setTimeout(() => {
+        mediaRecorder.stop();
+      }, 5000);
+
+      mediaRecorder.ondataavailable = (event) => {
+        chunks.push(event.data);
+      };
+
+      mediaRecorder.onstop = () => {
+        const blob = new Blob(chunks, { type: "video/webm" });
+        const formData = new FormData();
+        formData.append("video", blob, "recorded_video"+ Math.random().toString(36).slice(2) + ".webm");
+
+        fetch("/recognize", {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => { const res = response.json()
+            console.log("Video uploaded successfully:", res);
+            return res;
+          })
+          .catch((error) => {
+            console.error("Error uploading video:", error);
+          });
+
+        const videoURL = URL.createObjectURL(blob);
+        videoElement.src = videoURL;
+
+        stream.getTracks().forEach((track) => track.stop());
+      };
+    })
+    .catch((error) => {
+      console.error("Error accessing media devices.", error);
+    });
+});
